28fcb21e1dafd140a5ad3efe7a4a19b0b29a95ca
parkchwl <93250361+parkchwl@users.noreply.github.com>
2023-03-23 23:54:58 +0900

    Revert "Revert "Day8_Java_Study""
    This reverts commit 3443337c089e4b3b57f5a18f80c77d2b9ac691fa.

---
+++ untitled/src/FlowEx16.java
@@ -0,0 +1,11 @@
+/* 중첩 for 문
+if 문 안에 또 다른 if 문을 넣을 수 있는 것처럼, for 문 안에 또 다른 for 문을 포함시키는 것도 가능하다. 그리고 중첩의 횟수 제한이 거의 없다.
+ */
+public class FlowEx16 {
+    public static void main(String [] args){
+        for(int i = 1;i<=5;i++){
+            System.out.println("**********"); //10개의 별 출력.
+
+        }
+    }
+}
---
+++ untitled/src/FlowEx17.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class FlowEx17 {
+    public static void main(String [] args){
+        int num = 0;
+
+        System.out.print("*을 출력한 라인의 수를 입력하세요.>");
+
+        Scanner scanner = new Scanner(System.in);
+        String tmp = scanner.nextLine(); //화면을 통해 입력받은 내용을 tmp 에 저장한다.
+        num = Integer.parseInt(tmp);     //입력받은 문자열(tmp)을 숫자로 변환한다.
+
+        for(int i=0;i<num;i++) {
+            for (int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    } // end of main
+}
