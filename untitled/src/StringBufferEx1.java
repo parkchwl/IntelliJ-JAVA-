585c8a366755eca104889f299f493c85e4872e5e
parkchwl <93250361+parkchwl@users.noreply.github.com>
2023-04-13 15:47:16 +0900

    Day26_Java_Study
    by myself(java.lang)

--- untitled/src/StringBufferEx1.java
+++ untitled/src/StringBufferEx1.java
@@ -13,3 +13,14 @@ class StringBufferEx1 {
         System.out.println("s.equals(s2) ? " + s.equals(s2));
     }
 }
+/* String 클래스는 인스턴스를 생설할 때 지정된 문자열을 변경할 수 없지만 StringBuffer 클래스는 변경이 가능하다.
+내부적으로 문자열 편집을 위한 버퍼(buffer)를 가지고 있으며, StringBuffer인스턴스를 생성할 때 그 크기를 지정할 수 있다.
+이 때, 편집할 문자열의 길이를 고려하여 버퍼의 길이를 충분히 잡아주는 것이 좋다. 편집 중인 문자열이 버퍼의 길이를 넘어서게 되면
+버퍼의 길이를 늘려주는 작업이 추가로 수행되어야하기 때문에 작업효율이 떨어진다.
+StringBuffer클래스는 String 클래스와 유사한 점이 많다.
+
+#StringBuffer의 생성자.
+StringBuffer클래스의 인스턴스를 생성할 때, 적절한 길이의 char형 배열이 생성되고, 배열은 문자열을 저장하고 편집하기 위한 공간(buffer)으로 사용된다.
+StringBuffer인스턴스를 생성할 때는 생성자 StringBuffer(int length)를 사용해서 StringBuffer인스턴스에 저장될 문자열의 길이를 고려하여
+충분히 여유있는 크기로 지정하는 것이 좋다. StringBuffer인스턴스를 생성할 때, 버퍼의 크기를 지정해주지 않으면 16개의 문자를 저장할 수 있는 크기의 버퍼를 생성한다.
+ */
