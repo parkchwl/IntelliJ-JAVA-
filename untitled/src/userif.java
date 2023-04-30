b6a613e00450ecb1479b62b9356f7a0dabffe946
parkchwl <93250361+parkchwl@users.noreply.github.com>
2023-04-30 23:58:16 +0900

    userif

---
+++ untitled/src/userif.java
@@ -0,0 +1,19 @@
+public class userif {
+    public static void main(String[] args) {
+        int userInput = 2;
+
+        if (userInput == 1 || userInput == 2 || userInput == 3) {
+            if (userInput == 1) {
+                System.out.println("입력한 값은 1입니다.");
+            }
+
+            if (userInput == 2) {
+                System.out.println("입력한 값은 2입니다.");
+            }
+
+            if (userInput == 3) {
+                System.out.println("입력한 값은 3입니다.");
+            }
+        }
+    }
+}
